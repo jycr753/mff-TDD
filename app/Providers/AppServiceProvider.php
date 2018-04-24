08ace5116e14fd43cc1e44722b4b6b57dbf2bdcc
tanvir <tanvir.alam.shawn@gmail.com>
2018-04-24 15:24:54 +0200

    Make sure query value is passed only onece to view

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Channel;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /*\View::composer('threads.create', function ($view){
+           $view->with('channels', \App\Channel::all());
+        });*/
+
+        /*\View::composer('*', function ($view){
+            $view->with('channels', \App\Channel::all());
+        });*/
+
+        \View::share('channels', Channel::all());
+
     }
 
     /**
