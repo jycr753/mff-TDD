b76852d3287dfb3a585f8dc4a244793a397c1b22
tanvir <tanvir.alam.shawn@gmail.com>
2018-04-24 20:04:49 +0200

    Refactor the controller

---
+++ app/Filters/Filters.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Filters;
+
+use Illuminate\Http\Request;
+
+abstract class Filters
+{
+    protected $request, $builder;
+    protected $filters = [];
+
+    /**
+     * ThreadFilter constructor.
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+
+    /**
+     * @param $builder
+     * @return mixed
+     */
+    public function apply($builder)
+    {
+        $this->builder = $builder;
+
+        $this->getFilters()
+            ->filter(function($filter){
+                return method_exists($this, $filter);
+            })
+            ->each(function($filter, $value){
+               $this->$filter($value);
+            });
+
+        return $this->builder;
+    }
+
+    public function getFilters()
+    {
+        return collect($this->request->only($this->filters))->flip();
+    }
+}
