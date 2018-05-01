4a09c3187d390e57583167ad43cdaa947331d038
tanvir <tanvir.alam.shawn@gmail.com>
2018-05-01 15:03:08 +0200

    Delete a threa and replies and test cases

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -2,34 +2,36 @@
 
 @section('content')
     <div class="container">
-        <div class="page-header">
-            <h1>
-                {{ $profileUser->name }}
-                <small>Since {{ $profileUser->created_at->diffforhumans() }}</small>
-            </h1>
-        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="page-header">
+                    <h1>
+                        {{ $profileUser->name }}
+                        <small>Since {{ $profileUser->created_at->diffforhumans() }}</small>
+                    </h1>
+                </div>
 
-        @foreach($threads as $thread)
-            <div class="card">
-                <div class="card-header">
-                    <div class="level">
-                        <span class="flex">
-                            <a href="#">{{ $thread->creator->name }}</a> posted by
-                            {{ $thread->title }}
-                        </span>
-
-                        <span>
-                            {{ $thread->created_at->diffforhumans() }}
-                        </span>
+                @foreach($threads as $thread)
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="level">
+                                <span class="flex">
+                                    <a href="#">{{ $thread->creator->name }}</a> posted by
+                                    {{ $thread->title }}
+                                </span>
+                                <span>
+                                    {{ $thread->created_at->diffforhumans() }}
+                                </span>
+                                    </div>
+                                </div>
+                        <div class="card-body">
+                            {{ $thread->body }}
+                        </div>
                     </div>
-                </div>
+                @endforeach
 
-                <div class="card-body">
-                    {{ $thread->body }}
-                </div>
+                {{ $threads->links() }}
             </div>
-        @endforeach
-
-        {{ $threads->links() }}
+        </div>
     </div>
 @endsection
