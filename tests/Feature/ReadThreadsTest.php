10a7bd845319625a3bbb4149f27ecdd03b3bb1c7
tanvir <tanvir.alam.shawn@gmail.com>
2018-05-07 01:04:56 +0200

    Add a test case to see unanswered threads

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -31,17 +31,6 @@ class ReadThreadsTest extends TestCase
     }
 
     /** @test */
-    public function a_user_can_read_replies_to_a_thread()
-    {
-        $reply = create('App\Reply', [
-            'thread_id' => $this->thread->id
-        ]);
-
-        $this->get('/threads/' . $this->thread->channel . '/' . $this->thread->id)
-            ->assertSee($reply->body);
-    }
-
-    /** @test */
     public function a_user_can_filter_threads_according_to_a_channel()
     {
         $channel = create('App\Channel');
@@ -89,6 +78,17 @@ class ReadThreadsTest extends TestCase
     }
 
     /** @test */
+    public function a_user_can_filter_threads_that_are_unanswered()
+    {
+        $thread = create('App\Thread');
+        create('App\Reply', ['thread_id' => $thread->id]);
+
+        $response = $this->getJson('threads?unanswered=1')->json();
+
+        $this->assertCount(1, $response);
+    }
+
+    /** @test */
     public function a_user_can_request_all_replies_to_a_given_thread()
     {
         $thread = create('App\Thread');
@@ -96,7 +96,7 @@ class ReadThreadsTest extends TestCase
 
         $response = $this->getJson($thread->path() . '/replies')->json();
 
-        $this->assertCount(1, $response['data']);
+        $this->assertCount(2, $response['data']);
         $this->assertEquals(2, $response['total']);
     }
 }
