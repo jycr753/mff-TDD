571851a950a38f303273ab937b14bbc252229e4e
tanvir <tanvir.alam.shawn@gmail.com>
2018-05-07 01:01:42 +0200

    Add boot for suto increment and decrement replies count

--- app/Reply.php
+++ app/Reply.php
@@ -29,6 +29,22 @@ class Reply extends Model
      */
     protected $appends = ['favoriteCount', 'isFavorited'];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(
+            function ($reply) {
+                $reply->thread->increment('replies_count');
+            }
+        );
+
+        static::deleted(
+            function ($reply) {
+                $reply->thread->decrement('replies_count');
+            }
+        );
+    }
     /**
      * A reply has an owner
      *
