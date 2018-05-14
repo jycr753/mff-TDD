224964b7dc3b4c7bd735e31a1e4fe9f7151b4050
tanvir <tanvir.alam.shawn@gmail.com>
2018-05-14 04:31:58 +0200

    Structure the backend for marking a reply as the best reply

---
+++ app/Http/Controllers/BestRepliesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Reply;
+
+class BestRepliesController extends Controller
+{
+    /**
+     * Mark the best reply for a thread.
+     *
+     * @param  Reply $reply
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function store(Reply $reply)
+    {
+        $this->authorize('update', $reply->thread);
+
+        $reply->thread->markBestReply($reply);
+    }
+}
