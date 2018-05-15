360cf5aa9003ce8211cf575458aea5525ed62af0
tanvir <tanvir.alam.shawn@gmail.com>
2018-05-15 22:29:13 +0200

    Fix few things here and there

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -61,12 +61,11 @@ class ThreadsController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param Request $request
      * @param Recaptcha $recaptcha
      * 
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, Recaptcha $recaptcha)
+    public function store(Recaptcha $recaptcha)
     {
         request()->validate(
             [
